b2c96733e4ba9afa32e611ee1bf64f11e30f9fd1
Xunjian-Li <87350416+Xunjian-Li@users.noreply.github.com>
2025-05-04 14:26:02 -0700

    Update Cornucopia.jl

--- Cornucopia.jl
+++ Cornucopia.jl
@@ -52,7 +52,7 @@ function mle_gumbel(x::Vector)
         f = f - m * log(s1)
         df = df + m * s2 / s1
         d2f = -m / alpha^2 - m * s3 / s1 + m*(s2/s1)^2
-        alpha = alpha - df / d2f # Newton update
+        alpha = max(alpha - df / d2f, 1e-10) # Newton update
         #     println(iteration," ",f," ",alpha," ",df)
         if abs(df) < 1e-6
             break
